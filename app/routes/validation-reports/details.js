cf9983f04e6bc4b34d71ca944897acd7747b8147
anitacaron <anitarcaron@gmail.com>
2021-03-10 18:08:09 +0000

    detail validation view

--- app/routes/validation-reports/details.js
+++ app/routes/validation-reports/details.js
@@ -1,3 +1,9 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
-export default class ValidationReportsDetailsRoute extends Route {}
+export default class ValidationReportsDetailsRoute extends Route {
+  @service store;
+  model(params) {
+    return this.store.findRecord('validation-report', params.id);
+  }
+}
