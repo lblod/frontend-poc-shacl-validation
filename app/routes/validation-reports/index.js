f53531121863fb58d3355829a18d5a5ca0c602f5
anitacaron <anitarcaron@gmail.com>
2021-03-10 17:31:21 +0000

    validation reports view

--- app/routes/validation-reports/index.js
+++ app/routes/validation-reports/index.js
@@ -1,3 +1,9 @@
 import Route from '@ember/routing/route';
+// eslint-disable-next-line ember/no-mixins
+import DataTableRouteMixin from 'ember-data-table/mixins/route';
 
-export default class ValidationReportsIndexRoute extends Route {}
+export default class ValidationReportsIndexRoute extends Route.extend(
+  DataTableRouteMixin
+) {
+  modelName = 'validation-report';
+}
